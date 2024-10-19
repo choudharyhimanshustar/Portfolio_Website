eb12b941970471bd239c03417fe106486e8073be
Choudhary Himanshu <104679101+choudharyhimanshustar@users.noreply.github.com>
2024-10-19 14:08:46 +0530

    Update Experience.jsx

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -6,7 +6,7 @@ const Experience = () => {
   return (
     <div id="experience">
       <h2>
-        Experience
+        Skills
       </h2>
       <div className='experience_content'>
         <article className="card">
@@ -14,51 +14,37 @@ const Experience = () => {
           <h4>
             HTML
           </h4>
-          <small>
-            Proficient
-          </small>
+          
         </article><article className="card"><TiTick />
           <h4>
             CSS
           </h4>
-          <small>
-            Skilled
-          </small>
+          
         </article><article className="card"><TiTick />
           <h4>
             JS
           </h4>
-          <small>
-            Skilled
-          </small>
+          
         </article><article className="card"><TiTick />
           <h4>
             React
           </h4>
-          <small>
-            Skilled
-          </small>
+         
         </article><article className="card"><TiTick />
           <h4>
             NodeJS
           </h4>
-          <small>
-            Beginner
-          </small>
+          
         </article><article className="card"><TiTick />
           <h4>
             C++
           </h4>
-          <small>
-            Advanced
-          </small>
+         
         </article><article className="card"><TiTick />
           <h4>
             DSA
           </h4>
-          <small>
-            Skilled
-          </small>
+          
         </article>
       </div>
     </div>
